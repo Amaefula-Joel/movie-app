93c637c18dbfec9b9eaa76ce0c4f1612873e2700
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-01-18 10:15:59 +0100

    done with pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-// import logo from './logo.svg';
-import './App.css';
-import './styles/custom.css'; // Importing Bootstrap custom CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importing Bootstrap JS
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import Home from './pages/Home';
-import TrendingToday from './pages/TrendingToday';
-import TopRated from './pages/TopRated';
-import Theatres from './pages/Theatres';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+// import Trending from "./pages/Trending";
+// import TopRated from "./pages/TopRated";
+// import InTheatres from "./pages/InTheatres";
+// import TvSeries from "./pages/TvSeries";
+// import Bookmarks from "./pages/Bookmarks";
+// import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        {/* routes with sidebar */}
-        <Route path="/app" 
-        element={
-          <div style={{display:'flex'}}>
-            <Sidebar/>
-            <div style={{marginLeft:'250px',padding:'20px',flex:1}}>
-              <Routes>
-              <Route path="/about" element={<Home />} />
-              <Route path="/about" element={<TrendingToday />} />
-              <Route path="/services" element={<TopRated />} />
-              <Route path="/contact" element={<Theatres />} />
-              </Routes>
-            </div>
-          </div>
-        } /> 
-      </Routes>
-    </BrowserRouter>
+    <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/trending" element={<Trending />} />
+            <Route path="/top-rated" element={<TopRated />} />
+            <Route path="/in-theatres" element={<InTheatres />} />
+            <Route path="/tv-series" element={<TvSeries />} />
+            <Route path="/bookmarks" element={<Bookmarks />} /> */}
+          </Routes>
+    </Router>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+    <div className="container">
+      <div className="background">
+        <div className="flex justify-between mx-3">
+          <div className="title">
+            <a href="blah">
+              <h2>MovieFlix</h2>
+            </a>
+          </div>
+          <div className="search">
+            <input type="text" placeholder="search Movies" />
+          </div>
+        </div>
+        {/* end of header */}
+        <div className="items-center justify-center">
+          <h1 className="text-[50px] font-bold ">
+            <span className="text-blue-500 ">MovieFlix,</span>
+            Your Favourite Movie Finder
+          </h1>
+        </div>
+      </div>
+      {/* end of section */}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
