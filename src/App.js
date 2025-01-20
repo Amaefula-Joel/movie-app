83e66ff02cc488335e69869ecabfd32cbd390c60
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-01-20 09:02:25 +0100

    merges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Bookmarks from "./pages/Bookmarks";
-// import TrendingToday from "./pages/TrendingToday";
-// import TopRated from "./pages/TopRated";
-// import TvSeries from "./pages/TvSeries";
 import Theatres from "./pages/Theatres";
 
 
 function App() {
   return (
-    <Router>
-          <Routes>
-            <Route path="/" element={<Theatres />} />
-            {/* <Route path="/trending" element={<Trending />} />
-            <Route path="/top-rated" element={<TopRated />} />
-            <Route path="/in-theatres" element={<InTheatres />} />
-            <Route path="/tv-series" element={<TvSeries />} />
-            <Route path="/bookmarks" element={<Bookmarks />} /> */}
-          </Routes>
-    </Router>
+    <Theatres/>
   );
 }
 
